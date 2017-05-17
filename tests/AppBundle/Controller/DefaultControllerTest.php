e53946bb72005e180c12ebbb2952f0400513d116
dannebicque <david.annebicque@gmail.com>
2017-05-17 07:46:53 +0200

    Big update

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -13,6 +13,16 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+
+//        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+//        $stack = [];
+//        $this->assertEquals(0, count($stack));
+//
+//        array_push($stack, 'foo');
+//        $this->assertEquals('foo', $stack[count($stack)-1]);
+//        $this->assertEquals(1, count($stack));
+//
+//        $this->assertEquals('foo', array_pop($stack));
+//        $this->assertEquals(0, count($stack));
     }
 }
